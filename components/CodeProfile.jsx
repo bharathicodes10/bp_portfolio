c2e2cf5dd53bc56c1f555a27ebae8eb5940f7d48
priya <bharathipriyama@gmail.com>
2023-01-09 13:53:12 +0530

    style altered

--- components/CodeProfile.jsx
+++ components/CodeProfile.jsx
@@ -44,7 +44,7 @@ const CodeProfile = () => {
             </div>
           </div>
           <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+            <div className='grid grid-cols-2 gap-6 justify-center items-center'>
               <div className='m-auto'>
                 <a href="https://www.hackerrank.com/bharathi_10"><Image src={hackerrank} width='64px' height='64px' alt='/' /></a>
               </div>
