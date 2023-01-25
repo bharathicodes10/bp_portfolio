2f49e7c329f6ae995181657c1acf7cf7776a9a27
priya <bharathipriyama@gmail.com>
2023-01-25 06:27:29 +0530

    color change

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -175,7 +175,7 @@ const Navbar = () => {
               </Link>
             </ul>
             <div className='pt-40'>
-              <p className='uppercase tracking-widest text-[#5651e5]'>
+              <p className='uppercase tracking-widest text-cyan-300'>
                 Let&#39;s Connect
               </p>
               <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
